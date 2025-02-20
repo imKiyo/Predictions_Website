51fa5def202d9fefbec8d4223513adfa366e9b44
imKiyo <imKiyo@users.noreply.github.com>
2025-02-20 08:02:27 +0000

    deploy: 734d0add43121b94e5d9a45ed1fed49cdf379db3

--- keys.js
+++ keys.js
@@ -1,12 +1,12 @@
 const firebaseConfig = {
-  apiKey: "YOUR_FIREBASE_API_KEY",
-  authDomain: "YOUR_FIREBASE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://YOUR_DATABASE_URL",
-  projectId: "YOUR_FIREBASE_PROJECT_ID",
-  storageBucket: "YOUR_FIREBASE_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_FIREBASE_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "FIREBASE_API_KEY",
+  authDomain: "kiyo-predictions.firebaseapp.com",
+  databaseURL: "DATABASE_URL",
+  projectId: "FIREBASE_PROJECT_ID",
+  storageBucket: "kiyo-predictions.firebasestorage.app",
+  messagingSenderId: "SENDER_ID",
+  appId: "FIREBASE_APP_ID",
+  measurementId: "MEASUREMENT_ID"
 };
 
 // Initialize Firebase
