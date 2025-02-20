53cb06ad6cfc4ca4918e7dd0964c57cad519deae
imKiyo <xargon11@gmail.com>
2025-02-20 07:34:52 +0100

    fixed database calls i think

--- keys.js
+++ keys.js
@@ -1,10 +1,14 @@
 const firebaseConfig = {
   apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.PROJECT_ID_PLACEHOLDER + ".firebaseapp.com",
+  authDomain: process.env.FIREBASE_PROJECT_ID + ".firebaseapp.com",
+  databaseURL: process.env.DATABASE_URL,
   projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.PROJECT_ID_PLACEHOLDER + ".appspot.com",
-  messagingSenderId: process.env.SENDER_ID_PLACEHOLDER,
-  appId: process.env.FIREBASE_APP_ID
+  storageBucket: process.env.FIREBASE_PROJECT_ID + ".firebasestorage.app",
+  messagingSenderId: process.env.SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.MEASUREMENT_ID
 };
 
-export default firebaseConfig;;
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
