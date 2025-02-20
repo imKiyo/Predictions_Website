d5786e280e57814b13d1d99c6693525c349e5ea2
imKiyo <imKiyo@users.noreply.github.com>
2025-02-20 10:36:40 +0000

    deploy: 81bdc3ee69fe9111cdda8de09f625e841d4eea0f

--- keys.js
+++
@@ -1,1 +0,0 @@
-export const firebaseConfig={apiKey:process.env.FIREBASE_API_KEY,authDomain:"kiyo-predictions.firebaseapp.com",databaseURL:process.env.DATABASE_URL,projectId:process.env.FIREBASE_PROJECT_ID,storageBucket:"kiyo-predictions.appspot.com",messagingSenderId:process.env.SENDER_ID,appId:process.env.FIREBASE_APP_ID,measurementId:process.env.MEASUREMENT_ID};
